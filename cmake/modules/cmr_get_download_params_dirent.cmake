84e64b0789c326830922fcf60493010586b20113
NikitaFeodonit <nfeodonit@yandex.com>
2018-07-19 12:22:08 +0300

    Code refactoring. Move the common code part to LibCMaker.

--- cmake/modules/cmr_get_download_params_dirent.cmake
+++ cmake/modules/cmr_get_download_params_dirent.cmake
@@ -21,32 +21,26 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 # ****************************************************************************
 
-include(cmr_get_version_parts)
-include(cmr_print_fatal_error)
-
-function(cmr_dirent_get_download_params
-    version
-    out_url out_sha out_src_dir_name out_tar_file_name)
-
-  set(lib_base_url "https://github.com/tronkko/dirent/archive")
+# Part of "LibCMaker/cmake/modules/cmr_get_download_params.cmake".
 
   if(version VERSION_EQUAL "1.23.1")
-    set(lib_sha
+    set(arch_file_sha
       "8d699c068270e65e8cb2f76a3cd40542337fb984263dc6361bbb47e8ec89ea2a")
   endif()
 
-  if(NOT DEFINED lib_sha)
-    cmr_print_fatal_error("Library version ${version} is not supported.")
-  endif()
+  set(base_url "https://github.com/tronkko/dirent/archive")
+  set(src_dir_name    "dirent-${version}")
+  set(arch_file_name  "${src_dir_name}.tar.gz")
+  set(unpack_to_dir   "${unpacked_dir}/${src_dir_name}")
 
-  cmr_get_version_parts(${version} major minor patch tweak)
-
-  set(lib_src_name "dirent-${major}.${minor}.${patch}")
-  set(lib_tar_file_name "${lib_src_name}.tar.gz")
-  set(lib_url "${lib_base_url}/${major}.${minor}.${patch}.tar.gz")
-
-  set(${out_url} "${lib_url}" PARENT_SCOPE)
-  set(${out_sha} "${lib_sha}" PARENT_SCOPE)
-  set(${out_src_dir_name} "${lib_src_name}" PARENT_SCOPE)
-  set(${out_tar_file_name} "${lib_tar_file_name}" PARENT_SCOPE)
-endfunction()
+  set(${out_ARCH_SRC_URL}
+    "${base_url}/${major}.${minor}.${patch}.tar.gz" PARENT_SCOPE
+  )
+  set(${out_ARCH_DST_FILE}  "${download_dir}/${arch_file_name}" PARENT_SCOPE)
+  set(${out_ARCH_FILE_SHA}  "${arch_file_sha}" PARENT_SCOPE)
+  set(${out_SHA_ALG}        "SHA256" PARENT_SCOPE)
+  set(${out_UNPACK_TO_DIR}  "${unpack_to_dir}" PARENT_SCOPE)
+  set(${out_UNPACKED_SOURCES_DIR}
+    "${unpack_to_dir}/${src_dir_name}" PARENT_SCOPE
+  )
+  set(${out_VERSION_BUILD_DIR} "${build_dir}/${src_dir_name}" PARENT_SCOPE)
