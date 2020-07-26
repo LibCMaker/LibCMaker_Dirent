573b9d09af0f6b98251ec61a9c0b9caee7bb4041
NikitaFeodonit <nfeodonit@yandex.com>
2020-07-26 14:16:27 +0300

    Use CACHE vars in cmr_build_*.cmake

--- cmr_build_dirent.cmake
+++ cmr_build_dirent.cmake
@@ -29,9 +29,9 @@
 # Lib's name, version, paths
 #-----------------------------------------------------------------------
 
-set(DIRENT_lib_NAME      "Dirent")
-set(DIRENT_lib_VERSION   "1.23.1")
-set(DIRENT_lib_DIR       "${CMAKE_CURRENT_LIST_DIR}")
+set(DIRENT_lib_NAME "Dirent")
+set(DIRENT_lib_VERSION "1.23.1" CACHE STRING "DIRENT_lib_VERSION")
+set(DIRENT_lib_DIR "${CMAKE_CURRENT_LIST_DIR}" CACHE PATH "DIRENT_lib_DIR")
 
 # To use our Find<LibName>.cmake.
 list(APPEND CMAKE_MODULE_PATH "${DIRENT_lib_DIR}/cmake/modules")
@@ -41,7 +41,7 @@ list(APPEND CMAKE_MODULE_PATH "${DIRENT_lib_DIR}/cmake/modules")
 # LibCMaker_<LibName> specific vars and options
 #-----------------------------------------------------------------------
 
-set(COPY_DIRENT_CMAKE_BUILD_SCRIPTS ON)
+option(COPY_DIRENT_CMAKE_BUILD_SCRIPTS "COPY_DIRENT_CMAKE_BUILD_SCRIPTS" ON)
 
 
 #-----------------------------------------------------------------------
